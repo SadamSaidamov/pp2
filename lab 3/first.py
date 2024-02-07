57054104159beadc44256c051815db64123c40a9
SadamSaidamov <159142936+SadamSaidamov@users.noreply.github.com>
2024-02-07 21:08:02 +0600

    Delete lab 3 directory

--- lab 3/first.py
+++
@@ -1,9 +0,0 @@
-'''name=input("Please write the name: ")
-surname=input("Please write the surname: ")
-print(type(name))
-# print(f"Hello, {name} \n {surname}")
-print("Hello,", name , surname)
-'''
-
-fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(fruits[1:4])
