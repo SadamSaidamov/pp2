b0a57eaf4acb0041f95f5aa5b02675f16661f0aa
SadamSaidamov <159142936+SadamSaidamov@users.noreply.github.com>
2024-04-24 14:00:27 +0500

    Add files via upload

---
+++ lab10/Snake/Creating_table.py
@@ -0,0 +1,32 @@
+import psycopg2
+from config import config
+
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE Snake(
+            NickName VARCHAR(255) NOT NULL,
+            Level INTEGER DEFAULT 0,
+            Score INTEGER DEFAULT 0,
+            CONSTRAINT nickname UNIQUE (NickName)
+        )
+        """,
+        )
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        for command in commands:
+            cur.execute(command)
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    create_tables()
